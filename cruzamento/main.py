1bdd667bede5b9ef6077f14b3b2ed9b2a510bb21
Ítalo Alves <italooko@gmail.com>
2022-08-04 21:14:43 -0300

    Atualiza main

--- cruzamento/main.py
+++ cruzamento/main.py
@@ -1,6 +1,6 @@
 
-from servidor_central import inicia_servidor_central
-from servidor_distribuido import inicia_servidor_distribuido
+from servidor import Servidor
+from cruzamento import Cruzamento
 
 def main():
     print('----------------------------------------------------')
@@ -8,30 +8,36 @@ def main():
     print('----------------------------------------------------\n')
 
     print('Para comecar, digite a opcao de servidor que deseja utilizar:')
-    print('1 - Servidor Central')
-    print('2 - Servidor Distribuido 1')
-    print('3 - Servidor Distribuido 2')
-    print('0 - Sair\n')
+    print('0 - Servidor Central')
+    print('1 - Cruzamento 3')
+    print('2 - Cruzamento 4')
+    print('3 - Sair\n')
 
-    servidor_c1 = ('0.0.0.0', 10210)
-    servidor_d1 = ('164.41.98.17', 10201)
-    servidor_d2 = ('164.41.98.26', 10202)
+    servidor_central = ('164.41.98.26', 10210)
+    servidor_c1 = ('164.41.98.17', 10201)
+    servidor_c2 = ('164.41.98.17', 10202)
+    servidor_c3 = ('164.41.98.26', 10203)
+    servidor_c4 = ('164.41.98.26', 10204)
     
     opcao = -1
 
-    while opcao != 0:
+    while opcao != 3:
         opcao = int(input('Opcao: '))
 
-        if opcao == 1:
+        if opcao == 0:
             print('Voce escolheu o Servidor Central!\n')
-            inicia_servidor_central(servidor_c1, servidor_d1, servidor_d2)
+            #Cruzamento('1', *servidor_c1)
+            #Cruzamento('2', *servidor_c2)
+            Servidor(servidor_central, servidor_c1, servidor_c2, servidor_c3, servidor_c4)
+        elif opcao == 1:
+            print('Voce escolheu o Cruzamento 3!\n')
+            Cruzamento('3', *servidor_c3)
         elif opcao == 2:
-            print('Voce escolheu o Servidor Distribuido 1!\n')
-            inicia_servidor_distribuido(servidor_d1)
+            print('Voce escolheu o Cruzamento 4!\n')
+            Cruzamento('4', *servidor_c4)
         elif opcao == 3:
-            print('Voce escolheu o Servidor Distribuido 2!\n')
-            inicia_servidor_distribuido(servidor_d2)
-        elif opcao == 0:
             print('Saindo...\n')
         else:
             print('Opcao invalida')
+
+main()
